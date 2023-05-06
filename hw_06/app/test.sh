72b495d09825b2697d41460dc266b8d3aa478ef4
akieback <akieback7954@sdsu.edu>
2023-05-06 09:34:33 -0700

    source links

--- hw_06/app/test.sh
+++ hw_06/app/test.sh
@@ -3,6 +3,7 @@
 
 set -e
 
+# https://stackoverflow.com/questions/25503412/how-do-i-know-when-my-docker-mysql-container-is-up-and-mysql-is-ready-for-taking
 # Wait for MariaDB to start
 until mysqladmin ping -h mariadb -u root -proot -P3306 --silent; do
     echo "Waiting for MariaDB to start..."
@@ -15,6 +16,7 @@ echo "..."
 #DATABASE_EXISTS=$(mysql -h mariadb -u root -proot -P3306 -e "SHOW DATABASES LIKE 'baseball';" | grep baseball)
 #echo "$DATABASE_EXISTS"
 # Check if database exists
+# https://stackoverflow.com/questions/838978/how-to-check-if-mysql-database-exists
 if [[ ! -z "`mysql -h mariadb -u root -proot -P3306 -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='baseball'" 2>&1`" ]]; then
   echo "Database already exists"
 else
