a00b9a4196ee22ac17e6e772da37cc8c43ff22a2
akieback <akieback7954@sdsu.edu>
2023-05-05 17:07:34 -0700

    some fixes

--- hw_06/app/test.sh
+++ hw_06/app/test.sh
@@ -8,27 +8,28 @@ until mysqladmin ping -h mariadb -u root -proot -P3306 --silent; do
     echo "Waiting for MariaDB to start..."
     sleep 1
 done
+#mysql -h mariadb -u root -proot -P3306 -e "CREATE Database IF NOT EXISTS baseball;"
+echo "..."
 
 # Check if database exists
-DATABASE_EXISTS=$(mysql -h mariadb -u root -proot -P3306 -e "SHOW DATABASES LIKE 'baseball';" | grep baseball)
-
+#DATABASE_EXISTS=$(mysql -h mariadb -u root -proot -P3306 -e "SHOW DATABASES LIKE 'baseball';" | grep baseball)
+#echo "$DATABASE_EXISTS"
 # Check if database exists
-if [ "$DATABASE_EXISTS" == "baseball" ]; then
+if [[ ! -z "`mysql -h mariadb -u root -proot -P3306 -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME='baseball'" 2>&1`" ]]; then
   echo "Database already exists"
 else
   # Create the database
-  mysql -h mariadb -u root -proot -P3306 -e "CREATE Database baseball;"
+  mysql -h mariadb -u root -proot -P3306 -e "CREATE Database  IF NOT EXISTS baseball;"
   echo "Database created successfully1"
   #create database from baseball.sql file
   mysql -h mariadb -u root -proot -P3306 -D baseball < /app/baseball.sql
-  echo "Database created successfully"
+  echo "tables inserted"
 fi
-echo "tables inserted"
+echo "check completed"
 
-sleep 15
 mysql -h mariadb -u root -proot -P3306 -D baseball < /app/hw_05.sql
 echo "features created"
 #export final table to results.txt
-mysql -h mariadb -u root -proot -P3306 -Dbaseball -e "SELECT * FROM baseball.final_Stats;" > /results.txt
-
-echo "Database created successfully"
+mysql -h mariadb -u root -proot -P3306 -D baseball -e "SELECT * FROM final_Stats" > /results.txt
+echo "txt export done"
+echo "Container done"
